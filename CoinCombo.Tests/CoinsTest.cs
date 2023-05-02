8200b86557305bbedbf559655cb6c746be6fea38
Emma Gerigscott <elgerig@gmail.com>
2023-05-02 11:31:11 -0700

    add dime method to Coin Class, and add Nickel method to Coin Class
    Co-Authored-By Thomas McDowell <tmcdowellddhs@yahoo.com>
    Co-Authored-By Raed Alkhanbashi <Raed.Alkhanbashi@gmail.com>

--- CoinCombo.Tests/CoinsTest.cs
+++ CoinCombo.Tests/CoinsTest.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoinCombo.Models;
+using System.Collections.Generic;
+using System;
 
 namespace CoinCombo.Tests
 {
   [TestClass]
-  public class CoinsTest
+  public class CoinsTest : IDisposable
   {
+    public void Dispose()
+    {
+      Coin.ClearAll();
+    }
     [TestMethod]
     public void CoinClass_CreatesNewCoin_Coin()
     {
@@ -50,9 +56,46 @@ namespace CoinCombo.Tests
     public void QuarterMethod_AddsQuartersToList_String()
     {
       Coin newQuarter = new Coin();
-      string expected = "3 quarters";
-      float amount = 0.75F;
+      string expected = "3 quarter(s)";
+      decimal amount = 0.75m;
       newQuarter.Quarter(amount);
+      Assert.AreEqual(expected, Coin.Piggybank[0]);
+    }
+    // [TestMethod]
+    // public void QuarterMethod_ReturnsDecimalRemainder_Decimal()
+    // {
+    //   Coin newQuarter = new Coin();
+    //   decimal expected = 0.07m;
+    //   decimal amount = 1.32m;
+    //   decimal actual = newQuarter.Quarter(amount);
+    //   Assert.AreEqual(expected, actual);
+    // }
+
+    [TestMethod]
+    public void DimeMethod_AddsDimesToList_String()
+    {
+      Coin change = new Coin();
+      string expected = "1 dime(s)";
+      decimal amount = 1.42m;
+      change.Quarter(amount);
+      Assert.AreEqual(expected, Coin.Piggybank[1]);
+    }
+
+    [TestMethod]
+    public void DimeMethod_RetunrDecimalRemainder_Decimal()
+    {
+      Coin change = new Coin();
+      decimal expected = 0.07m;
+      decimal amount = 1.47m;
+      Assert.AreEqual(expected, change.Dime(amount));
+    }
+        [TestMethod]
+    public void NickelMethod_AddsNickelToList_String()
+    {
+      Coin change = new Coin();
+      string expected = "1 nickel(s)";
+      decimal amount = 0.07m;
+      change.Nickel(amount);
       Assert.AreEqual(expected, Coin.Piggybank[0]);
     }
   }
--- CoinCombo/Models/Coins.cs
+++ CoinCombo/Models/Coins.cs
@@ -6,10 +6,51 @@ namespace CoinCombo.Models
 {
   public class Coin {
     public static List<string> Piggybank { get; set; } = new List<string> {};
-    public void Quarter(float amount)
+    public void Quarter(decimal amount)
     {
-      int quotient = (int)(Math.Floor(amount / 0.25F));
-      Piggybank.Add(quotient.ToString() + " quarters");
+      decimal quarter = 0.25m;
+      int quotient = (int)(Math.Floor(amount / quarter));
+      Piggybank.Add(quotient.ToString() + " quarter(s)");
+      decimal remainder = amount % quarter;
+      remainder = Decimal.Round(remainder, 2);
+      Dime(remainder); 
+    }
+    public decimal Dime(decimal r)
+    {
+      decimal dime = 0.10m;
+      if (r < dime)
+      {
+        return r;
+      }
+      else 
+      {
+      int quotient = (int)(Math.Floor(r / dime));
+      Piggybank.Add(quotient.ToString() + " dime(s)");
+      decimal remainder = r % dime;
+      remainder = Decimal.Round(remainder, 2);
+      return remainder; 
+      }
+    }
+        public decimal Nickel(decimal r)
+    {
+      decimal nickel = 0.05m;
+      if (r < nickel)
+      {
+        return r;
+      }
+      else 
+      {
+      int quotient = (int)(Math.Floor(r / nickel));
+      Piggybank.Add(quotient.ToString() + " nickel(s)");
+      decimal remainder = r % nickel;
+      remainder = Decimal.Round(remainder, 2);
+      return remainder; 
+      }
+    }
+
+    public static void ClearAll()
+    {
+      Piggybank.Clear();
     }
   }
 }
