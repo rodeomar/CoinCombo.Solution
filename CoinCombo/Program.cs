e4c74b10ff22981919736874f4b6032032611854
Emma Gerigscott <elgerig@gmail.com>
2023-05-02 12:01:17 -0700

    Add Initial UI Console lines
    Co-Authored-By Thomas McDowell <tmcdowellddhs@yahoo.com>
    Co-Authored-By Raed Alkhanbashi <Raed.Alkhanbashi@gmail.com>

--- CoinCombo/Program.cs
+++ CoinCombo/Program.cs
@@ -1,7 +1,19 @@
-class Program
+using System;
+using CoinCombo.Models;
+
+namespace CoinCombo
 {
-  static void Main()
-  {
-    
-  }
+    class Program
+    {
+        static void Main()
+        {
+          Console.WriteLine("Welcome to our Change Maker");
+          Console.WriteLine("Enter a Dolla amount(EG: '1.37')");
+          string userAmount = Console.ReadLine();
+          Console.WriteLine("Calculating...");
+          Console.WriteLine("The results are in!!!!!!");
+          
+
+        }
+    }
 }
